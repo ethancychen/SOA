6edec470383e566038cee19919b22fc682b3229e
ethancychen <gatheringbc@yahoo.com.tw>
2014-10-03 02:57:13 +0800

    done with the exhausting hw3

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,7 +1,16 @@
 require_relative 'tsv_buddy'
+require 'yaml'
 
 class FlipFlap
+include TsvBuddy
+
+def take_yaml(yml)
+	@data = YAML::load(yml)
+end
+
+def to_yaml
+	@data.to_yaml
+end
 
   # Do not create any initializers
-
 end
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,4 +1,4 @@
-# Module that can be included (mixin) to create and parse TSV data
+# Module that can be included (mixin) to create and parse TSV datamodule TsvBuddy
 module TsvBuddy
   # @data should be a data structure that stores information
   #  from TSV or Yaml files. For example, it could be an Array of Hashes.
@@ -7,12 +7,17 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    require 'CSV'
+    csv = CSV.new(tsv, :headers => true, :col_sep => "\t")
+    @data = csv.to_a.map {|row| row.to_hash }
   end
-
+  
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    string = ''
+    string << @data[0].keys.join("\t") + "\n"
+    0.upto(data.count-1) {|i| string << @data[i].values.join("\t") + "\n"}
+    string
   end
 end

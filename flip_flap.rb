95feb2834432fc6ca1f703d74f05701e347c975a
ethancychen <gatheringbc@yahoo.com.tw>
2014-10-03 21:39:35 +0800

    v2 of hw3

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,16 +1,14 @@
 require_relative 'tsv_buddy'
 require 'yaml'
-
+# FilpFlap
 class FlipFlap
-include TsvBuddy
+  include TsvBuddy
+  def take_yaml(yml)
+    @data = YAML.load(yml)
+  end
 
-def take_yaml(yml)
-	@data = YAML::load(yml)
-end
-
-def to_yaml
-	@data.to_yaml
-end
-
+  def to_yaml
+    @data.to_yaml
+  end
   # Do not create any initializers
 end
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,4 +1,5 @@
-# Module that can be included (mixin) to create and parse TSV datamodule TsvBuddy
+# Module that can be included (mixin) to create
+# and parse TSV datamodule TsvBuddy
 module TsvBuddy
   # @data should be a data structure that stores information
   #  from TSV or Yaml files. For example, it could be an Array of Hashes.
@@ -8,16 +9,15 @@ module TsvBuddy
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
     require 'CSV'
-    csv = CSV.new(tsv, :headers => true, :col_sep => "\t")
-    @data = csv.to_a.map {|row| row.to_hash }
+    csv = CSV.new(tsv, headers: true, col_sep: "\t")
+    @data = csv.to_a.map(&:to_hash)
   end
-  
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
     string = ''
     string << @data[0].keys.join("\t") + "\n"
-    0.upto(data.count-1) {|i| string << @data[i].values.join("\t") + "\n"}
+    0.upto(data.count - 1) { |i| string << @data[i].values.join("\t") + "\n" }
     string
   end
 end
